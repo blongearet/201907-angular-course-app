1894bfa510415844b32b029a59dd274cb1caeb94
Benjamin Longearet <firehist@gmail.com>
2019-03-19 12:29:49 +0100

    step-06-2 / Create an @Input rating property

--- src/app/shared/ui/star/star.component.ts
+++ src/app/shared/ui/star/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-star',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./star.component.scss']
 })
 export class StarComponent implements OnInit {
+
+  @Input() public rating: number = 0
 
   constructor() { }
 
