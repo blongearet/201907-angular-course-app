a22d54646a2f68e1120599a8add191dbd5754011
Benjamin Longearet <firehist@gmail.com>
2019-03-21 08:29:44 +0100

    step-09-3 / Get products froml the API

--- src/app/shared/model/product/product.service.ts
+++ src/app/shared/model/product/product.service.ts
@@ -1,70 +1,28 @@
 import { Injectable } from '@angular/core';
 import {IProduct, Product} from './product';
+import {HttpClient} from '@angular/common/http';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private products: Product[]
+  private products: Product[] = []
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.fetch()
+  }
 
-    const productFromAPI: IProduct[] = [
-      {
-        "id": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-      },
-      {
-        "id": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      },
-      {
-        "id": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-      },
-      {
-        "id": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2016",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-      },
-      {
-        "id": 10,
-        "productName": "Video Game Controller",
-        "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2015",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "starRating": 4.6,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-      }
-    ]
-
-    this.products = productFromAPI.map(product => new Product(product))
-
+  public fetch(): void {
+    this.http.get('http://localhost:3000/products')
+      .pipe(
+        map((products: IProduct[]) => {
+          return products.map((product: IProduct) => new Product(product))
+        }),
+        tap((products: Product[]) => console.log(`Here we got ${products.length} products!`))
+      )
+      .subscribe((products: Product[]) => this.products = products)
   }
 
   public getProducts(): Product[] {
