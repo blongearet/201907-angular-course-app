7eaca74208b79a11b0ee365e6ff67f050cea493e
Benjamin Longearet <firehist@gmail.com>
2019-03-22 10:39:57 +0100

    step-11-5-5 / Use CanActivate guard onto protected routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ProductDetailComponent} from './product/product-detail/product-detail.component';
 import {ProductComponent} from './product/product/product.component';
 import {ProductListComponent} from './product/product-list/product-list.component';
@@ -7,23 +7,26 @@ import {ProductEditComponent} from './product/product-edit/product-edit.componen
 import {WelcomeComponent} from './welcome/welcome.component';
 import {IdIsValidIdGuard} from './shared/guard/id-is-valid-id-guard.service';
 import {LoginComponent} from './auth/login/login.component';
+import {UserLoginGuard} from './shared/guard/user-login.guard';
+import {UserUnloggedGuard} from './shared/guard/user-unlogged.guard';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: WelcomeComponent},
-  {path: 'login', component: LoginComponent},
+  {path: '', pathMatch: 'full', component: WelcomeComponent, canActivate: [UserLoginGuard]},
+  {path: 'login', component: LoginComponent, canActivate: [UserUnloggedGuard]},
   {
     path: 'products',
-    component: ProductComponent, children: [
-      { path: '', component: ProductListComponent},
-      { path: ':id/edit', component: ProductEditComponent, canActivate: [IdIsValidIdGuard]},
-      { path: ':id', component: ProductDetailComponent, canActivate: [IdIsValidIdGuard]}
+    component: ProductComponent, canActivate: [UserLoginGuard], children: [
+      {path: '', component: ProductListComponent},
+      {path: ':id/edit', component: ProductEditComponent, canActivate: [IdIsValidIdGuard]},
+      {path: ':id', component: ProductDetailComponent, canActivate: [IdIsValidIdGuard]}
     ]
   },
-  { path: '**', pathMatch: 'full', redirectTo: ''}
+  {path: '**', pathMatch: 'full', redirectTo: ''}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
