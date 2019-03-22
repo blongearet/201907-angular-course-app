f87d22497e8be26fed1e745d6266cedf21a44e6d
Benjamin Longearet <firehist@gmail.com>
2019-03-22 15:13:40 +0100

    step-12-2 / Inject FormBuilder into the ProductEditComponent

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(fb: FormBuilder) { }
 
   ngOnInit() {
   }
