d70ab6be04e5e80943da7e989e4559ad6a98028d
Benjamin Longearet <firehist@gmail.com>
2019-03-22 15:33:22 +0100

    step-12-4 / Create FormGroup/FormControl

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+const HTTP_URL_PATTERN: string =
+  '^((http[s]?):\\/)\\/?([^:\\/\\s]+)((\\/\\w+)*)([\\w\\-\\.]+[^#?\\s]+)(.*)?(#[\\w\\-]+)?$'
 
 @Component({
   selector: 'app-product-edit',
@@ -10,7 +13,18 @@ export class ProductEditComponent implements OnInit {
 
   public productForm: FormGroup
 
-  constructor(fb: FormBuilder) { }
+  constructor(fb: FormBuilder) {
+    this.productForm = fb.group({
+      id: [null],
+      productName: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(80)]],
+      productCode: [''],
+      releaseDate: [new Date()],
+      description: [''],
+      price: [0, Validators.min(0)],
+      starRating: [0, [Validators.min(0), Validators.max(5)]],
+      imageUrl: ['', Validators.pattern(HTTP_URL_PATTERN)]
+    })
+  }
 
   ngOnInit() {
   }
