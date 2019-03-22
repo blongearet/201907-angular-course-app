70004382d4df63e0ddd5ff1dde3a1f5611b45b93
Benjamin Longearet <firehist@gmail.com>
2019-03-22 08:20:33 +0100

    step-11-3 / Display details

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs';
+import {Product} from '../../shared/model/product/product';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {ProductService} from '../../shared/model/product/product.service';
-import {filter, map, tap} from 'rxjs/operators';
+import {filter, map, switchMap, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,11 +12,14 @@ import {filter, map, tap} from 'rxjs/operators';
 })
 export class ProductDetailComponent {
 
+  // Our current product information as an Observable
+  public product$: Observable<Product>
+
   constructor(productService: ProductService, route: ActivatedRoute) {
     // First of all, we construct the Observable to get id from url
     // We'll use the paramMap which is a ParamMap
     // cf: https://angular.io/api/router/ParamMap
-    const id$: Observable<number> = route.paramMap
+    const id$ = route.paramMap
       .pipe(
         // We transform the ParamMap into a string which is the id
         map((paramMap: ParamMap) => paramMap.get('id')),
@@ -26,6 +30,15 @@ export class ProductDetailComponent {
         // Just a console.log
         tap((id: number) => console.log(`id is ${id}`))
       )
+
+    // Let's retrieve our product Observable based on previous id Observable
+    // It means that's a new event on id$ will trigger an event here an update
+    //  as well our product
+    this.product$ = id$.pipe(
+      // We switch the OBservable to the product one to retrieve
+      // cf: https://www.learnrxjs.io/operators/transformation/switchmap.html
+      switchMap((id: number) => productService.getProductById(id))
+    )
   }
 
 }
