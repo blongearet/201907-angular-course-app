737a9b18983dba4d359058748bc9543221bdbd41
Benjamin Longearet <firehist@gmail.com>
2019-03-22 08:00:13 +0100

    step-10-2 / Set routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProductDetailComponent} from './product/product-detail/product-detail.component';
+import {ProductComponent} from './product/product/product.component';
+import {ProductListComponent} from './product/product-list/product-list.component';
+import {ProductEditComponent} from './product/product-edit/product-edit.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {HeaderComponent} from './layout/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HeaderComponent, outlet: 'header'},
+  {path: '', pathMatch: 'full', component: WelcomeComponent},
+  {
+    path: 'products',
+    component: ProductComponent, children: [
+      { path: '', component: ProductListComponent},
+      { path: ':id/edit', component: ProductEditComponent},
+      { path: ':id', component: ProductDetailComponent}
+    ]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ProductSearchPipe } from './product/product-search.pipe';
 import {HttpClientModule} from '@angular/common/http';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product/product.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ProductDetailComponent } from './product/product-detail/product-detail.
     ProductEditComponent,
     WelcomeComponent,
     ProductSearchPipe,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
